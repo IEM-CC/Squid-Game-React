833f19a7fb90f24f20a4cc56d964fce91457a761
[Kaustav Giri] <[kaustavgiri2017@gmail.com]>
2023-02-28 00:06:01 +0530

    Added Quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Name from "./components/Main/Name";
+import Quiz from "./components/Quiz/Quiz";
 
 export default function App() {
   return (
     <div className="h-screen w-screen overflow-hidden bg-[#FB3E64] flex justify-center items-center">
       <Routes>
         <Route path="/" element={<Name/>} />
+        <Route path="/quiz" element={<Quiz />}/>   
       </Routes>
     </div>
   )
---
+++ src/components/Quiz/QuestionArea.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const QuestionArea = () => {
+  return (
+    <div>QuestionArea</div>
+  )
+}
+
+export default QuestionArea
